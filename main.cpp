3d0cac6bdad918592611282e9d19a20d18502381
Murat Vuran <uratvuran1905@gmail.com>
2026-05-07 03:56:59 +0300

    SFML altyapisi icin CMakeLists ve main.cpp hazirlandi

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
 int main() {
-    std::cout << "Dungeon Crawler projesi basliyor!" << std::endl;
+    // 800x600 boyutlarinda bir pencere olusturuyoruz
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Dungeon Crawler - Murat Vuran");
+
+    // Oyun dongusu: Pencere acik oldugu surece calisir
+    while (window.isOpen()) {
+        sf::Event event;
+        // Kullanici carpiya basarsa pencereyi kapatma mantigi
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Ekrani tamamen siyaha boya (temizle)
+        window.clear(sf::Color::Black);
+        
+        // Ileride karakterlerimizi buraya cizecegiz
+        
+        // Ekrani goster
+        window.display();
+    }
+
     return 0;
 }
